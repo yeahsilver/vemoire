544a5b72865cc8c7c93782f3e152bd5e77089ac0
Ubuntu <ubuntu@ip-172-31-44-216.ap-northeast-2.compute.internal>
2020-03-30 16:10:32 +0000

    from aws

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
+var createError = require('http-errors');
 var express = require('express');
-var app = express(); // app 객체 생성
-console.log('process.env.PORT', process.env.PORT);
-app.get('/', function(req,res){
-    res.send("Hello, World");
-})
-app.listen(process.env.PORT, function(){
-    console.log('Connected!!!');
-})
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// port setup
+app.set('port', process.env.PORT || 9000);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+var server = app.listen(app.get('port'), function(){
+console.log('Express server listening on port '+server.address().port);
+});
